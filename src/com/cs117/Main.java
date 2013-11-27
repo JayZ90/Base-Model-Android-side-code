f8b4b89657e63de52a47ac86eccbbb413edafea1
Amy Lin <amy_y_lin@outlook.com>
2013-11-27 02:46:11 -0800

    more files

--- src/com/cs117/Main.java
+++ src/com/cs117/Main.java
@@ -12,6 +12,19 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import com.cs117.R;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.UUID;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+
 public class Main extends Activity implements SensorEventListener {
 	
 	private float mLastX, mLastY, mLastZ;
@@ -31,7 +44,19 @@ public class Main extends Activity implements SensorEventListener {
     private final String forward = "Drone is moving FORWARD";
     private final String backward = "Drone is moving BACKWARD";
     private final String none = "Drone is waiting for command";
-	 
+
+	private static final int REQUEST_ENABLE_BT = 1;
+	private BluetoothAdapter btAdapter = null;
+	private BluetoothSocket btSocket = null;
+	private OutputStream outStream = null; 
+	
+	// Well known SPP UUID
+	private static final UUID MY_UUID =
+			UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+	
+	// Insert your server's MAC address
+	private static String address = "C4:85:08:5A:1B:E3";
+    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -41,7 +66,74 @@ public class Main extends Activity implements SensorEventListener {
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         mSensorManager.registerListener(this, mAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);
+        
+        moveDir = none;
+        
+		btAdapter = BluetoothAdapter.getDefaultAdapter();
+		CheckBTState();
+		
+        // Set up a pointer to the remote node using it's address.
+        BluetoothDevice device = btAdapter.getRemoteDevice(address);
+
+
+        // Two things are needed to make a connection:
+        //   A MAC address, which we got above.
+        //   A Service ID or UUID.  In this case we are using the
+        //     UUID for SPP.
+        try {
+            btSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
+        } catch (IOException e) {
+            AlertBox("Fatal Error", "In onResume() and socket create failed: " + e.getMessage() + ".");
+        }
+
+        // Discovery is resource intensive.  Make sure it isn't going on
+        // when you attempt to connect and pass your message.
+        btAdapter.cancelDiscovery();
+        
+        // Establish the connection.  This will block until it connects.
+        try {
+            btSocket.connect();
+            System.out.println("\n...Connection established and data link opened...");
+        } catch (IOException e) {
+            try {
+                if(btSocket.isConnected())
+                    btSocket.close();
+            } catch (IOException e2) {
+                AlertBox("Fatal Error", "In onResume() and unable to close socket during connection failure" + e2.getMessage() + ".");
+            }
+        }
+        
+        try {
+            outStream = btSocket.getOutputStream();
+        } catch (IOException e) {
+            AlertBox("Fatal Error", "In onResume() and output stream creation failed:" + e.getMessage() + ".");
+        }
     }
+    
+	private void CheckBTState() {
+		// Check for Bluetooth support and then check to make sure it is turned on
+		
+		// Emulator doesn't support Bluetooth and will return null 
+		if(btAdapter==null) {
+			AlertBox("Fatal Error", "Bluetooth Not supported. Aborting.");
+		}
+		else if(!btAdapter.isEnabled()) { 
+			//Prompt user to turn on Bluetooth
+			Intent enableBtIntent = new Intent(btAdapter.ACTION_REQUEST_ENABLE);
+			startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT); 
+		}
+	}
+	
+	public void AlertBox( String title, String message ){
+		new AlertDialog.Builder(this)
+		.setTitle( title )
+		.setMessage( message + " Press OK to exit." )
+		.setPositiveButton("OK", new OnClickListener() {
+			public void onClick(DialogInterface arg0, int arg1) {
+				finish();
+				} 
+			}).show();
+	}
 
     protected void onResume() {
         super.onResume();
@@ -51,6 +143,23 @@ public class Main extends Activity implements SensorEventListener {
     protected void onPause() {
         super.onPause();
         mSensorManager.unregisterListener(this);
+        
+        /*
+        if (btSocket.isConnected()) {
+
+            try {
+                outStream.flush();
+            } catch (IOException e) {
+                AlertBox("Fatal Error", "In onPause() and failed to flush output stream: " + e.getMessage() + ".");
+            }
+
+            try {
+                btSocket.close();
+            } catch (IOException e2) {
+                Log.d("30","--Pause failed to close socket");
+                AlertBox("Fatal Error", "In onPause() and failed to close socket." + e2.getMessage() + ".");
+            }
+        }*/
     }
 
 	@Override
@@ -120,7 +229,24 @@ public class Main extends Activity implements SensorEventListener {
 			}
 			
 			
-			
+	        //if(btSocket.isConnected())
+	        if(moveDir != none)
+	        {	
+	            // Create a data stream so we can talk to server.
+	            System.out.println("\n...Sending message to server...");
+	            
+	            byte[] msgBuffer = moveDir.getBytes();
+	            try {
+	                outStream.write(msgBuffer);
+	            } catch (IOException e) {
+	                String msg = "In onResume() and an exception occurred during write: " + e.getMessage();
+	                if (address.equals("00:00:00:00:00:00"))
+	                    msg = msg + ".\n\nUpdate your server address from 00:00:00:00:00:00 to the correct address on line 37 in the java code";
+	                msg = msg +  ".\n\nCheck that the SPP UUID: " + MY_UUID.toString() + " exists on server.\n\n";
+
+	                AlertBox("Fatal Error", msg);
+	            }
+	        }
 			
 			/* Method 2: 
 			
